ec40a43a34055bf9eeb37303eecbe577b5bdb523
Dell <Levanhuytricker@gmail.com>
2019-11-27 15:45:44 +0700

    Update

---
+++ app/src/main/java/com/example/duan1_huylvph08000/dictonary/DictonaryPresenter.java
@@ -0,0 +1,14 @@
+package com.example.duan1_huylvph08000.dictonary;
+
+public class DictonaryPresenter implements DictonaryMVP.Presnter {
+DictonaryMVP.View view;
+
+    public DictonaryPresenter(DictonaryMVP.View view) {
+        this.view = view;
+    }
+
+    @Override
+    public void clickedGoHome() {
+       view.goHome();
+    }
+}
